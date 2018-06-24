f24ed20be0f27f51faa18f76a3fce69ac86bba41
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-06-24 18:56:38 +0430

    Define types of variables

--- First.js
+++ First.js
@@ -11,3 +11,29 @@ function learningJavascript(){
     // Type casting is automatic --> You can write text = 2; in next line and it's totally ok!
     document.getElementById("p1") = text;
 }
+
+
+function defineVars(){
+    var name = "Mohsen"; // String
+    var age = 20; // Integer
+    var height = 73.3; // Double or Float
+    var bigInt = 12e10; // 12 * 10^10
+    var isMale = true; // Boolean
+
+    var array = new Array(); // Array Class --> Make an object using new keyword
+    // Array can have multiple types in its elements --> [Int, Double, String, Array, Object, ...]
+    array[0] = 1;
+    array[1] = "Naghipourfar";
+
+    var names = new Array("Ali", "Majid", "Mohsen", "Mohammad") // Another definition on array
+    var lastnames = ["Naghipourfar", "Ahmadi", "Lukako", "Ronaldo"] // Another definition of array
+
+    var student = ["Mohsen", 20, 183, 73.3] // it is ok too!
+
+    // define an object --> Like json objects --> Inspiration for json
+    var programmer = {
+        name : "Mohsen",
+        lastname: "Naghipourfar",
+        age : 20
+    }
+}
