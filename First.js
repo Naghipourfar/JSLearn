4ca1265691a1fbf060cd163a7e8f26a51b02c9d5
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-06-24 18:41:19 +0430

    Define Variables

--- First.js
+++ First.js
@@ -1,3 +1,13 @@
+var name = "Mohsen" // Global Variable
+
+
 function changeText(){
     document.getElementById("p1").innerHTML = "The function in js file is just invoked!";
+}
+
+function learningJavascript(){
+    // Define Variables --> JS is weakly structured!
+    var text = "Hi, My name is Mohsen Naghipourfar"; // Local Variable
+    // Type casting is automatic --> You can write text = 2; in next line and it's totally ok!
+    document.getElementById("p1") = text;
 }
